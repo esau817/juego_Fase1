a7b5667628857a3edc1eac005e0b288dbf3638d9
esau817 <36904834+esau817@users.noreply.github.com>
2019-09-02 22:44:02 -0500

    Add files via upload

---
+++ juego_Script.js
@@ -0,0 +1,111 @@
+var myGamePiece;
+
+function startGame() {
+    myGamePiece = new component(30, 30, "new_Character.png", 10, 240, "image");
+    myBackground = new component(656, 500, "laberinto.jpg", 0, 0, "image");
+    myGameArea.start();
+}
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 656;
+        this.canvas.height = 500;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.frameNo = 0;
+        this.interval = setInterval(updateGameArea, 20);
+        },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop : function() {
+        clearInterval(this.interval);
+    }
+}
+
+function component(width, height, color, x, y, type) {
+    this.type = type;
+    if (type == "image") {
+        this.image = new Image();
+        this.image.src = color;
+    }
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.x = x;
+    this.y = y;    
+    this.update = function() {
+        ctx = myGameArea.context;
+        if (type == "image") {
+            ctx.drawImage(this.image, 
+                this.x, 
+                this.y,
+                this.width, this.height);
+        } else {
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;        
+    }
+}
+
+function updateGameArea() {
+    myGameArea.clear();
+    myGamePiece.newPos();
+    myGamePiece.update();
+}
+
+function moveup() {
+    myGamePiece.speedY = -1; 
+}
+
+function movedown() {
+    myGamePiece.speedY = 1; 
+}
+
+function moveleft() {
+    myGamePiece.speedX = -1; 
+}
+
+function moveright() {
+    myGamePiece.speedX = 1; 
+}
+
+function clearmove() {
+    myGamePiece.speedX = 0; 
+    myGamePiece.speedY = 0; 
+}
+
+var myGamePiece;
+var myBackground;
+
+
+function updateGameArea() {
+  myGameArea.clear();
+  myBackground.newPos(); 
+  myBackground.update();
+  myGamePiece.newPos(); 
+  myGamePiece.update();
+}
+
+function checkBox(){
+    let vector = document.getElementById("txtDir").value;
+    String(vector).toLowerCase();
+
+    if (vector=="up") {
+        moveup();      
+    }
+    else if (vector=="down") {
+        movedown();
+    } else if (vector=="left"){
+        moveleft();
+    }
+    else if (vector=="right"){
+        moveright();
+    }
+}
